d47464b927c265d83ecad9555d4ab27b017c6d98
Tushar Agey <agey.tushar3@gmail.com>
2026-04-18 17:08:11 +0530

    update base

--- magic-ui/vite.config.ts
+++ magic-ui/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/magic-ui/",
   plugins: [react()],
   resolve: {
     alias: {
